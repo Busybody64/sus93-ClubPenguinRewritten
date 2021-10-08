53b9142f1ad856551ac20220ba2896ce5cc113a0
Busybody64 <92069026+Busybody64@users.noreply.github.com>
2021-10-08 09:56:29 -0500

    uploaded file

--- ClubPenguinRewritten.js
+++ ClubPenguinRewritten.js
@@ -1,12 +1,12 @@
 (async function(){
 $alert({msg:'To see more of the screen, rezize the window, or put it in fullscreen.',title:"Note",icon:"https://i.imgur.com/zzz1KA1.png"});
-  $window({
+ $window({
     icon: "apps/chat.gif",
-    title: "Your World Of Text",
+    title: "Club Penguin Rewritten",
     width: 500,
     height: 470,
     animationIn: "",
     animationOut: "hinge",
-    url: "https://www.yourworldoftext.com"
+    url: "https://play.cprewritten.net/"
   })
 })
